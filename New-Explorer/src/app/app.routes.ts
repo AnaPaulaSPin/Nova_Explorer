14e455bb30c0ba50b8518dea2ba874466c75950b
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-09-02 12:25:56 -0300

    Criação de um icon para projeto

--- New-Explorer/src/app/app.routes.ts
+++ New-Explorer/src/app/app.routes.ts
@@ -8,32 +8,32 @@ export const routes: Routes = [
 	},
 	{
 		path: 'pagina-inicial',
-		title: 'Página Inicial - Nova Explorer',
+		title: 'Página Inicial',
 		loadComponent: () => import('./pages/pagina-inicial/pagina-inicial.component').then((m) => m.PaginaInicialComponent)
 	},
 	{
 		path: 'galeria',
-		title: 'Galeria - Nova Explorer',
+		title: 'Galeria',
 		loadComponent: () => import('./pages/galeria/galeria.component').then((m) => m.GaleriaComponent)
 	},
 	{
 		path: 'meus-arquivos',
-		title: 'Meus Arquivos - Nova Explorer',
+		title: 'Meus Arquivos',
 		loadComponent: () => import('./pages/meus-arquivos/meus-arquivos.component').then((m) => m.MeusArquivosComponent)
 	},
 	{
 		path: 'pesquisa-inteligente',
-		title: 'Pesquisa Inteligente - Nova Explorer',
+		title: 'Pesquisa Inteligente',
 		loadComponent: () => import('./pages/pesquisa-inteligente/pesquisa-inteligente.component').then((m) => m.PesquisaInteligenteComponent)
 	},
 	{
 		path: 'tags',
-		title: 'Tags - Nova Explorer',
+		title: 'Tags',
 		loadComponent: () => import('./pages/tags/tags.component').then((m) => m.TagsComponent)
 	},
 	{
 		path: 'timeline',
-		title: 'Timeline - Nova Explorer',
+		title: 'Timeline',
 		loadComponent: () => import('./pages/timeline/timeline.component').then((m) => m.TimelineComponent)
 	},
 	{
